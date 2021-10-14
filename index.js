7930aa8d26b37a91306e04d576a5fd8b411dff90
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-10-14 02:42:22 -0400

    Refactor for Dev Experience & User Schema
    Created postResolvers to get post data
    Created Mutation of RegisterInput
        to add new user to db and there User Schema

--- index.js
+++ index.js
@@ -1,39 +1,11 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose"); // orm lib, lets us interface with mongoDB
-
-const Post = require("./models/Post.js");
-
-const { MONGODB } = require("./config.js");
-
 const PORT = process.env.PORT || 5000;
 
-const typeDefs = gql`
-	type Post {
-		id: ID!
-		body: String!
-		createdAt: String!
-		username: String!
-	}
-	# Gets all "Post" from our db and goes to collection post and bring all documents from there and returns it to user
-	type Query {
-		getPosts: [Post]
-	}
-`;
-
-const resolvers = {
-	Query: {
-		async getPosts() {
-			// Try catch to handle errors so server doesn't shutdown
-			try {
-				const posts = await Post.find();
-				return posts;
-			} catch (err) {
-				throw new Error(err);
-			}
-		},
-	},
-};
+const typeDefs = require("./graphql/typeDefs.js");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const server = new ApolloServer({
 	typeDefs,
