0099ea529b8edf9094f4e5f7b93ba40729c491a2
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2021-10-14 01:26:02 -0400

    Connected ApolloServer and MongoDB &  Schema Model
    Created Schema
    Created Resolvers
    Created Type Definitions
    
    Secured .env and .config files

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-import React from 'react'
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose"); // orm lib, lets us interface with mongoDB
 
-export default function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Post = require("./models/Post.js");
+
+const { MONGODB } = require("./config.js");
+
+const PORT = process.env.PORT || 5000;
+
+const typeDefs = gql`
+	type Post {
+		id: ID!
+		body: String!
+		createdAt: String!
+		username: String!
+	}
+	# Gets all "Post" from our db and goes to collection post and bring all documents from there and returns it to user
+	type Query {
+		getPosts: [Post]
+	}
+`;
+
+const resolvers = {
+	Query: {
+		async getPosts() {
+			// Try catch to handle errors so server doesn't shutdown
+			try {
+				const posts = await Post.find();
+				return posts;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+mongoose
+	.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("MongoDB Connected");
+		return server.listen({ port: PORT });
+	})
+	.then((res) => {
+		console.log(`Server running at ${res.url}`);
+	})
+	.catch((err) => {
+		console.error(err);
+	});
