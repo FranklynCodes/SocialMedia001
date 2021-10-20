dd2b3a89e77416e9d8ce56ee3bed473591ed253e
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-10-19 20:59:50 -0400

    Added Total like & comment count using .length
    in resolvers/index.js
        If change any of the field, each time a querry, mutation or a
        subscription occurs it will go through the Post modifers and apply
        them. Like middlewear. Post func has a higher order of precidence

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,16 @@ const usersResolvers = require("./users");
 const commentsResolvers = require("./comments");
 
 module.exports = {
+	// If change any of the field, each time a querry, mutation or a subscription occurs it will go through the Post modifers and apply them. Kind of like middlewear, Post func has a higher order of precidence
+	Post: {
+		// likeCount:(parent) // parent holds data from previous steps
+		// might have to swap this to arrow notation, cut the return
+		likeCount(parent) {
+			console.log("parent:", parent);
+			return parent.likes.length;
+		},
+		commentCount: (parent) => parent.comments.length,
+	},
 	Query: {
 		...postsResolvers.Query,
 	},
