c6d3298ab26981d8682ce387fab1f98805a2507f
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-10-19 21:59:45 -0400

    Squash me

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -38,6 +38,10 @@ module.exports = {
 			const user = checkAuth(context);
 			console.log(user);
 
+			if (args.body.trim() === "") {
+				throw new Error("Post body must not be empty");
+			}
+
 			const newPost = new Post({
 				body,
 				user: user.id,
