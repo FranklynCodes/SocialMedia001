befcfd8251a2baa473b156c1cf5bd663a74920d9
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-10-21 19:51:19 -0400

    Connected Querry to React FrontEnd for Post & Home
    Home.js = Mapped posts values from FETCH_POSTS_Querry
        to post as props which then get passed down to PostCard Component
    
    Added GraphQL querry to FETCH POSTS in MongoDB

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import { Grid } from "semantic-ui-react";
+
+import PostCard from "../components/PostCard";
 
 function Home() {
+	const { loading, data: { getPosts: posts } = {} } = useQuery(FETCH_POSTS_QUERY); // Careful posts can be null or undefined, create a case for that
+
 	return (
-		<div>
-			<h1>Home Page</h1>
-		</div>
+		<Grid columns={3}>
+			{/* Watch out for specificailty issues */}
+			<Grid.Row id="page-title">
+				<h1>Recent Posts</h1>
+			</Grid.Row>
+			<Grid.Row>
+				{loading ? (
+					<h1>Loading posts..</h1>
+				) : (
+					// posts = data, data converted to post, passed down as props to Components
+					posts &&
+					posts.map((post) => (
+						<Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+							<PostCard post={post} />
+						</Grid.Column>
+					))
+				)}
+			</Grid.Row>
+		</Grid>
 	);
 }
+
+const FETCH_POSTS_QUERY = gql`
+	{
+		getPosts {
+			id
+			body
+			createdAt
+			username
+			likeCount
+			likes {
+				username
+			}
+			commentCount
+			comments {
+				id
+				username
+				createdAt
+				body
+			}
+		}
+	}
+`;
 
 export default Home;
