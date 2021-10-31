9ee1311d36533e88f089432764d61a250b39b032
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2021-10-30 22:24:44 -0400

    Fixed Routes

--- client/src/util/AuthRoute.js
+++ client/src/util/AuthRoute.js
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
-import { AuthContext } from "../context/auth";
 
+import { AuthContext } from "../context/auth";
 // pass down route component
 function AuthRoute({ component: Component, ...rest }) {
-	const user = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
+
 	return (
 		<Route
 			{...rest}
-			render={(props) => (user ? <Redirect to="/" /> : <Component {...props}></Component>)}
-		></Route>
+			render={(props) => (user ? <Redirect to="/" /> : <Component {...props} />)}
+		/>
 	);
 }
 
